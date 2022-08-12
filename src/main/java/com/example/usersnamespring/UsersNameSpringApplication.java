1bd727aa9c05daba1ce30d2c49823bc62ca94d1d
Vperedivverh <105232340+Vperedivverh@users.noreply.github.com>
2022-08-12 15:21:11 +0300

    перенос на спринг

--- src/main/java/com/example/usersnamespring/UsersNameSpringApplication.java
+++ src/main/java/com/example/usersnamespring/UsersNameSpringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UsersNameSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UsersNameSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UsersNameSpringApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/usersnamespring/repository/UserRepository.java
+++ src/main/java/com/example/usersnamespring/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package com.example.usersnamespring.repository;public interface UserRepository {
+package com.example.usersnamespring.repository;
+
+import com.example.usersnamespring.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
